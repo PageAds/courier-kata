4e5a03582b7391441b33c9945d4986be73551d03
Adam Page <adampage6@gmail.com>
2018-12-18 21:01:03 +0000

    Defined Step 5 (discounts) test methods

---
+++ CourierKata.Tests/WhenWorkingWithParcelOrder.cs
@@ -0,0 +1,48 @@
+﻿using Xunit;
+
+namespace CourierKata.Tests
+{
+    public class WhenWorkingWithParcelOrder
+    {
+
+        [Fact]
+        public void CalculateOrderCost_WhenAnOrderIsApplicableForSmallParcelMania_Every4thSmallParcelIsFreeAndCostIsReturned()
+        {
+            //Arrange
+
+            //Act
+
+            //Assert
+        }
+
+        [Fact]
+        public void CalculateOrderCost_WhenAnOrderIsApplicableForMediumParcelMania_Every3rdMediumParcelIsFreeAndCostIsReturned()
+        {
+            //Arrange
+
+            //Act
+
+            //Assert
+        }
+
+        [Fact]
+        public void CalculateOrderCost_WhenAnOrderIsApplicableForMixedParcelMania_Every5thParcelIsFreeAndCostIsReturned()
+        {
+            //Arrange
+
+            //Act
+
+            //Assert
+        }
+
+        [Fact]
+        public void CalculateOrderCost_WhenAnOrderIsApplicableForMultipleDiscounts_BestPriceDiscountsAreSelectedAndCostIsReturned()
+        {
+            //Arrange
+
+            //Act
+
+            //Assert
+        }
+    }
+}
