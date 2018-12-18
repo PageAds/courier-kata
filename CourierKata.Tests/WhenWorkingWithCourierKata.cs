be8587ddd06828fac6183f7d905a432bd6f2c446
Adam Page <adampage6@gmail.com>
2018-12-18 19:27:56 +0000

    Defined Step 1 (initial implementation) test methods

--- CourierKata.Tests/WhenWorkingWithCourierKata.cs
+++ CourierKata.Tests/WhenWorkingWithCourierKata.cs
@@ -1,6 +1,47 @@
-﻿namespace CourierKata.Tests
+﻿using Xunit;
+
+namespace CourierKata.Tests
 {
     public class WhenWorkingWithCourierKata
     {
+        [Fact]
+        public void CalculateCost_SmallParcel_3DollarsIsReturned()
+        {
+            //Arrange
+
+            //Act
+
+            //Assert
+        }
+
+        [Fact]
+        public void CalculateCost_MediumParcel_8DollarsIsReturned()
+        {
+            //Arrange
+
+            //Act
+
+            //Assert
+        }
+
+        [Fact]
+        public void CalculateCost_LargeParcel_15DollarsIsReturned()
+        {
+            //Arrange
+
+            //Act
+
+            //Assert
+        }
+
+        [Fact]
+        public void CalculateCost_XLParcel_25DollarsIsReturned()
+        {
+            //Arrange
+
+            //Act
+
+            //Assert
+        }
     }
 }
