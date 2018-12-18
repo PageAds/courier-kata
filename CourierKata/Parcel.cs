655133c7b50c07987b51f8693278f47180d6a91d
Adam Page <adampage6@gmail.com>
2018-12-18 19:44:53 +0000

    Fixed Step 1 (initial implementation) test methods

--- CourierKata/Parcel.cs
+++ CourierKata/Parcel.cs
@@ -1,15 +1,61 @@
-﻿namespace CourierKata
+﻿using System;
+
+namespace CourierKata
 {
     public class Parcel
     {
+        private ParcelType parcelType;
+
+        /// <summary>
+        /// A parcel
+        /// </summary>
+        /// <param name="dimensions">Total size of all dimensions of the parcel in cm</param>
         public Parcel(double dimensions)
         {
-                
+            parcelType = this.CalculateParcelType(dimensions);
         }
 
         public decimal CalculateCost()
         {
-            return 0;
+            switch (parcelType)
+            {
+                case ParcelType.Small:
+                    return 3;
+                case ParcelType.Medium:
+                    return 8;
+                case ParcelType.Large:
+                    return 15;
+                case ParcelType.XL:
+                    return 25;
+                default:
+                    throw new Exception($"Unable to determine cost for Parcel Type: {parcelType}");
+
+            }
+        }
+
+        private ParcelType CalculateParcelType(double dimensions)
+        {
+            if (dimensions < 10)
+            {
+                return ParcelType.Small;
+            }
+
+            if (dimensions < 50)
+            {
+                return ParcelType.Medium;
+            }
+
+            if (dimensions < 100)
+            {
+                return ParcelType.Large;
+            }
+
+            if (dimensions >= 100)
+            {
+                return ParcelType.XL;
+            }
+
+            throw new Exception($"Unable to determine Parcel Type based on dimensions: {dimensions}cm");
         }
     }
 }
